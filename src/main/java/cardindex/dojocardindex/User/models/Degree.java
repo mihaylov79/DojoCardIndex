d578ce5702679c6f79e83e17e75a934f912c72bc
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-02 13:04:57 +0200

    Changes in Enum class Description Fixed  org.attoparser.ParseException in all-users-list.html Added dropDown to ${reachedDegree} in admin-user-edit.html

--- src/main/java/cardindex/dojocardindex/User/models/Degree.java
+++ src/main/java/cardindex/dojocardindex/User/models/Degree.java
@@ -1,46 +1,38 @@
 package cardindex.dojocardindex.User.models;
 
 public enum Degree {
-    KYU_10(10,"кю"),
-    KYU_9(9,"кю"),
-    KYU_8(8,"кю"),
-    KYU_7(7,"кю"),
-    KYU_6(6,"кю"),
-    KYU_5(5,"кю"),
-    KYU_4(4,"кю"),
-    KYU_3(3,"кю"),
-    KYU_2(2,"кю"),
-    KYU_1(1,"кю"),
-    DAN_1(1,"дан"),
-    DAN_2(2,"дан"),
-    DAN_3(3,"дан"),
-    DAN_4(4,"дан"),
-    DAN_5(5,"дан"),
-    DAN_6(6,"дан"),
-    DAN_7(7,"дан"),
-    DAN_8(8,"дан"),
-    DAN_9(9,"дан");
-
-    private final int level;
-    private final String rank;
+    KYU_10("10 кю"),
+    KYU_9("9 кю"),
+    KYU_8("8 кю"),
+    KYU_7("7 кю"),
+    KYU_6("6 кю"),
+    KYU_5("5 кю"),
+    KYU_4("4 кю"),
+    KYU_3("3 кю"),
+    KYU_2("2 кю"),
+    KYU_1("1 кю"),
+    DAN_1("1 дан"),
+    DAN_2("2 дан"),
+    DAN_3("3 дан"),
+    DAN_4("4 дан"),
+    DAN_5("5 дан"),
+    DAN_6("6 дан"),
+    DAN_7("7 дан"),
+    DAN_8("8 дан"),
+    DAN_9("9 дан");
 
 
-    Degree(int level, String rank) {
-        this.level = level;
-        this.rank = rank;
+    private final String description;
 
+
+    Degree(String description) {
+        this.description = description;
     }
 
-    public int getLevel() {
-        return level;
+
+    public String getDescription() {
+        return description;
     }
 
-    public String getRank() {
-        return rank;
-    }
 
-    @Override
-    public String toString() {
-        return level + " " + rank;
-    }
 }
