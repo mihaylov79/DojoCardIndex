b0203c61212fcb7c567735988a510673cc51b6e7
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-23 08:56:00 +0200

    Removed Unnecessary Setters Logic

--- src/main/java/cardindex/dojocardindex/Event/models/Event.java
+++ src/main/java/cardindex/dojocardindex/Event/models/Event.java
@@ -87,24 +87,8 @@ public class Event {
         return firstPlaceWinner;
     }
 
-
     public void setFirstPlaceWinner(User firstPlaceWinner) {
-        if(this.type == EventType.TOURNAMENT) {
-            this.firstPlaceWinner = firstPlaceWinner;
-        }else {
-            throw new IllegalArgumentException("Победители могат да бъдат добавяни само в събития тип Турнир");
-        }
-        if (!users.contains(firstPlaceWinner)) {
-            throw new IllegalArgumentException("Победителя трявбва да бъде участник в събитието.");
-        }
-        if (this.firstPlaceWinner != null) {
-            this.firstPlaceWinner.setAchievedFirstPlaces(this.firstPlaceWinner.getAchievedFirstPlaces() - 1);
-        }
         this.firstPlaceWinner = firstPlaceWinner;
-        if (firstPlaceWinner != null) {
-            firstPlaceWinner.setAchievedFirstPlaces(firstPlaceWinner.getAchievedFirstPlaces() + 1);
-        }
-
     }
 
     public User getSecondPlaceWinner() {
@@ -112,22 +96,7 @@ public class Event {
     }
 
     public void setSecondPlaceWinner(User secondPlaceWinner) {
-        if (this.type == EventType.TOURNAMENT) {
-            this.secondPlaceWinner = secondPlaceWinner;
-        }else {
-            throw new IllegalArgumentException("Победители могат да бъдат добавяни само в събития тип Турнир");
-        }
-        if (!users.contains(secondPlaceWinner)) {
-            throw new IllegalArgumentException("Победителя трявбва да бъде участник в събитието.");
-        }
-        if (this.secondPlaceWinner != null) {
-            this.secondPlaceWinner.setAchievedSecondPlaces(this.secondPlaceWinner.getAchievedSecondPlaces() - 1);
-        }
         this.secondPlaceWinner = secondPlaceWinner;
-        if (secondPlaceWinner != null) {
-            secondPlaceWinner.setAchievedSecondPlaces(secondPlaceWinner.getAchievedSecondPlaces() + 1);
-        }
-
     }
 
     public User getThirdPlaceWinner() {
@@ -135,23 +104,7 @@ public class Event {
     }
 
     public void setThirdPlaceWinner(User thirdPlaceWinner) {
-        if (this.type == EventType.TOURNAMENT) {
-            this.thirdPlaceWinner = thirdPlaceWinner;
-        }else {
-            throw new IllegalArgumentException("Победители могат да бъдат добавяни само в събития тип Турнир");
-        }
-
-        if (!users.contains(thirdPlaceWinner)) {
-            throw new IllegalArgumentException("Победителя трявбва да бъде участник в събитието.");
-        }
-        if (this.thirdPlaceWinner != null) {
-            this.thirdPlaceWinner.setAchievedThirdPlaces(this.thirdPlaceWinner.getAchievedThirdPlaces() - 1);
-        }
         this.thirdPlaceWinner = thirdPlaceWinner;
-        if (thirdPlaceWinner != null) {
-            thirdPlaceWinner.setAchievedThirdPlaces(thirdPlaceWinner.getAchievedThirdPlaces() + 1);
-        }
-
     }
 
     public String getLocation() {
