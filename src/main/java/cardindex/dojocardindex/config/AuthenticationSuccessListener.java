d401cc10d9425ca7509d899d5de9d021ec98781e
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-24 12:23:26 +0200

    Added AuthenticationSuccessListener - fixed problem accessing NotificationPreference page for users without save Preference

---
+++ src/main/java/cardindex/dojocardindex/config/AuthenticationSuccessListener.java
@@ -0,0 +1,31 @@
+package cardindex.dojocardindex.config;
+
+import cardindex.dojocardindex.User.models.User;
+import cardindex.dojocardindex.User.service.UserService;
+import cardindex.dojocardindex.notification.service.NotificationService;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSuccessListener {
+
+    private final NotificationService notificationService;
+    private final UserService userService;
+
+    public AuthenticationSuccessListener(NotificationService notificationService, UserService userService) {
+        this.notificationService = notificationService;
+        this.userService = userService;
+    }
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        // Вземаме текущия потребител от SecurityContext
+        String email = event.getAuthentication().getName();
+        User user = userService.findUserByEmail(email);
+
+        // Извикваме метода за проверка на предпочитанията за известия
+        notificationService.checkNotificationPreference(user.getId(), user.getEmail());
+    }
+}
+
