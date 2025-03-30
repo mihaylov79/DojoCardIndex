cfae19a50b02a615a7614f6860708509ae6d8da2
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-31 02:05:58 +0300

    More Tests Added

--- src/test/java/cardindex/dojocardindex/web/mapper/DTOMapperUTest.java
+++ src/test/java/cardindex/dojocardindex/web/mapper/DTOMapperUTest.java
@@ -3,9 +3,10 @@ package cardindex.dojocardindex.web.mapper;
 import cardindex.dojocardindex.Event.models.Event;
 import cardindex.dojocardindex.Event.models.EventType;
 import cardindex.dojocardindex.Event.models.Requirements;
-import cardindex.dojocardindex.User.models.User;
+import cardindex.dojocardindex.User.models.*;
 import cardindex.dojocardindex.web.dto.EditEventRequest;
 import cardindex.dojocardindex.web.dto.EditUserProfileRequest;
+import cardindex.dojocardindex.web.dto.UserEditAdminRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -49,7 +50,7 @@ public class DTOMapperUTest {
     }
 
     @Test
-    void mapEventToEditEventRequest_happyPath(){
+    void given_mapEventToEditEventRequest_happyPath(){
 
         Event event = Event.builder()
                 .id(UUID.randomUUID())
@@ -71,4 +72,49 @@ public class DTOMapperUTest {
         assertEquals(event.getLocation(),dto.getLocation());
         assertEquals(event.getRequirements(),dto.getRequirements());
     }
+
+    @Test
+    void given_mapUserToUserEditAdminRequest_happyPath(){
+
+        User user = User.builder()
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .userPhone("0888888888")
+                .profilePicture("www.picdata.com")
+                .birthDate(LocalDate.parse("1980-05-03"))
+                .role(UserRole.ADMIN)
+                .status(UserStatus.ACTIVE)
+                .registrationStatus(RegistrationStatus.REGISTERED)
+                .reachedDegree(Degree.NONE)
+                .ageGroup(AgeGroup.M)
+                .isCompetitor(false)
+                .medicalExamsPassed(LocalDate.parse("2025-02-02"))
+                .interests("none")
+                .height(180)
+                .weight(75)
+                .contactPerson("Georgi Ivanov")
+                .contactPersonPhone("0888888887")
+                .build();
+
+        UserEditAdminRequest dto = DTOMapper.mapUserToUserEditAdminRequest(user);
+
+        assertEquals(user.getBirthDate(),dto.getBirthDate());
+        assertEquals(user.getUserPhone(),dto.getUserPhone());
+        assertEquals(user.getRole(),dto.getRole());
+        assertEquals(user.getStatus(),dto.getStatus());
+        assertEquals(user.getRegistrationStatus(),dto.getRegistrationStatus());
+        assertEquals(user.getReachedDegree(),dto.getReachedDegree());
+        assertEquals(user.getIsCompetitor(),dto.getIsCompetitor());
+        assertEquals(user.getAgeGroup(),dto.getAgeGroup());
+        assertEquals(user.getHeight(),dto.getHeight());
+        assertEquals(user.getWeight(),dto.getWeight());
+        assertEquals(user.getMedicalExamsPassed(),dto.getMedicalExamsPassed());
+        assertEquals(user.getContactPerson(),dto.getContactPerson());
+        assertEquals(user.getContactPersonPhone(),dto.getContactPersonPhone());
+
+    }
+
+
+
+
 }
