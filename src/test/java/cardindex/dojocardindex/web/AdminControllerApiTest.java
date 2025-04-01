3e7c8d7efc5bd1c7e50ac169f398d1325e16a2a7
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-04-01 09:17:46 +0300

    More Tests Added

--- src/test/java/cardindex/dojocardindex/web/AdminControllerApiTest.java
+++ src/test/java/cardindex/dojocardindex/web/AdminControllerApiTest.java
@@ -15,6 +15,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -97,7 +99,7 @@ public class AdminControllerApiTest {
 
     @Test
     public void addNewUser_WithAdminRoleAndValidData_ShouldRedirectToHome() throws Exception {
-        // 1. Подготовка на тестови данни
+
         UUID userId = UUID.randomUUID();
         CustomUserDetails userDetails = new CustomUserDetails(
                 userId,
@@ -146,5 +148,57 @@ public class AdminControllerApiTest {
 
         SecurityContextHolder.clearContext();
     }
+
+    @Test
+    public void getRegisterRequests_WithAdminRole_ShouldReturnViewWithRequests() throws Exception {
+
+       UUID detailsId = UUID.randomUUID();
+       CustomUserDetails userDetails = new CustomUserDetails(
+               detailsId,
+               "admin@example.com",
+               "123321",
+               UserRole.ADMIN,
+               RegistrationStatus.REGISTERED,
+               UserStatus.ACTIVE
+       );
+
+       Authentication auth = new UsernamePasswordAuthenticationToken(
+               userDetails,
+               null,
+               userDetails.getAuthorities()
+       );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+
+        User mockUser = User.builder()
+                .id(detailsId)
+                .email("admin@example.com")
+                .registrationStatus(RegistrationStatus.REGISTERED)
+                .status(UserStatus.ACTIVE)
+                .build();
+        User request1 = User.builder()
+                .email("user1@test.com")
+                .registrationStatus(RegistrationStatus.REGISTERED)
+                .status(UserStatus.ACTIVE)
+                .build();
+        User request2 = User.builder()
+                .email("user2@test.com")
+                .registrationStatus(RegistrationStatus.REGISTERED)
+                .status(UserStatus.ACTIVE)
+                .build();
+        List<User> mockRequests = Arrays.asList(request1, request2);
+
+        when(userService.getUserById(detailsId)).thenReturn(mockUser);
+        when(userService.getRegisterRequests()).thenReturn(mockRequests);
+
+        mockMvc.perform(get("/admin/register-requests"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register-requests"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attribute("user", mockUser))
+                .andExpect(model().attributeExists("registerRequests"))
+                .andExpect(model().attribute("registerRequests", mockRequests));
+    }
+
 
 }
