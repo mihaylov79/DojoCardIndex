d1694570db285c7664899bd69aa48a1df924e75e
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-09-18 09:46:52 +0300

    api link - updated

--- src/main/java/cardindex/dojocardindex/notification/client/NotificationClient.java
+++ src/main/java/cardindex/dojocardindex/notification/client/NotificationClient.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "mail-svc", url = "http://localhost:8081/api/v1/notifications")
+@FeignClient(name = "mail-svc", url = "https://mail-svc-app-container-app.livelysea-10642e14.switzerlandnorth.azurecontainerapps.io/api/v1/notifications")
 public interface NotificationClient {
 
     @PostMapping("/preferences")
