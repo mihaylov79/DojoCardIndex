7eefb0f8174949a02e26bad1aea2a164d6fe6639
Mihaylov <misho.mihaylov@gmail.com>
2025-09-20 01:34:06 +0300

    Terraform updated

--- deployment/main.tf
+++ deployment/main.tf
@@ -61,6 +61,15 @@ resource "azurerm_subnet" "subnet" {
   name                 = "dragon-doje-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 resource "azurerm_mysql_flexible_server" "server" {
