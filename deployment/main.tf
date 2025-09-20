cdf9e509efb5d4adda1c4323ea49d152344c7e14
Mihaylov <misho.mihaylov@gmail.com>
2025-09-20 16:58:01 +0300

    Terraform updated

--- deployment/main.tf
+++ deployment/main.tf
@@ -99,7 +99,7 @@ resource "azurerm_mysql_flexible_database" "db" {
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "fr" {
-  end_ip_address      = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
   name                = "dojo-firewall"
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.server.name
