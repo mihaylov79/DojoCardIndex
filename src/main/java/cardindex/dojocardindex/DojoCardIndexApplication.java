ce8ff77d0b5908e2da58d444eb5adabd09a064c2
Mihaylov <misho.mihaylov@gmail.com>
2025-10-03 00:35:20 +0300

    updated

--- src/main/java/cardindex/dojocardindex/DojoCardIndexApplication.java
+++ src/main/java/cardindex/dojocardindex/DojoCardIndexApplication.java
@@ -2,6 +2,7 @@ package cardindex.dojocardindex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableCaching
 public class DojoCardIndexApplication {
 
     public static void main(String[] args) {
