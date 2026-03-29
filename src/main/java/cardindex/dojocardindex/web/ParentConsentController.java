9fedead5adba7f6a197c1f88e84b7299a55935d3
Mihaylov <misho.mihaylov@gmail.com>
2026-03-29 05:53:41 +0300

    added page with Agreement and option to refuse

--- src/main/java/cardindex/dojocardindex/web/ParentConsentController.java
+++ src/main/java/cardindex/dojocardindex/web/ParentConsentController.java
@@ -4,10 +4,9 @@ package cardindex.dojocardindex.web;
 import cardindex.dojocardindex.UserConsent.model.UserConsent;
 import cardindex.dojocardindex.UserConsent.service.UserConsentService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/parent-consent")
@@ -20,11 +19,35 @@ public class ParentConsentController {
     }
 
     @GetMapping("/{token}")
-    public ModelAndView handleParentConsent(@PathVariable String token) {
+    public ModelAndView showParentConsentForm(@PathVariable String token) {
+        UserConsent consent = userConsentService.getConsentByToken(token);
+        ModelAndView modelAndView = new ModelAndView("parent-consent-form");
+        modelAndView.addObject("agreement", consent.getAgreement());
+        modelAndView.addObject("token", token);
+        return modelAndView;
+    }
+
+    @PostMapping("/{token}/accept")
+    public ModelAndView acceptParentConsent(@PathVariable String token) {
         UserConsent verifiedConsent = userConsentService.verifyParentConsent(token);
         ModelAndView modelAndView = new ModelAndView("parent-consent-success");
         modelAndView.addObject("agreementTitle", verifiedConsent.getAgreement().getTitle());
         modelAndView.addObject("consentId", verifiedConsent.getId());
         return modelAndView;
+    }
+
+    @GetMapping("/{token}/refuse")
+    public ModelAndView showRefuseWarning(@PathVariable String token) {
+        ModelAndView modelAndView = new ModelAndView("parent-consent-refuse-warning");
+        modelAndView.addObject("token", token);
+        return modelAndView;
+    }
+
+    @PostMapping("/{token}/refuse")
+    public String refuseParentConsent(@PathVariable String token, RedirectAttributes redirectAttributes) {
+        UserConsent consent = userConsentService.getConsentByToken(token);
+        userConsentService.refuseConsent(consent.getUser());
+        redirectAttributes.addFlashAttribute("errorMessage", "Родителското съгласие е отказано. Акаунтът ще бъде деактивиран.");
+        return "redirect:/parent-consent-fail";
     }
 }
