bcc3d3875b30ef3b5a97c5a6b9ec5163eb69dfef
mihaylov79 <misho.mihaylov@gmail.com>
2026-09-11 01:22:37 +0300

    Edited main.tf - changed resource "azurerm_mysql_flexible_server_firewall_rule" "fr" end_ip_address      = "0.0.0.0" ---- resource "azurerm_linux_web_app" "alwa" https_only          = true ---------- app_settings - "SPRING_DATASOURCE_URL" - requireSSL=true --------- changed application-prod.properties added: server.forward-headers-strategy=framework -------- server.servlet.session.cookie.secure=true server.servlet.session.cookie.http-only=true server.servlet.session.cookie.same-site=lax

--- deployment/main.tf
+++ deployment/main.tf
@@ -38,14 +38,14 @@ resource "azurerm_service_plan" "sp" {
 
 # resource "azurerm_virtual_network" "vn" {
 #   location            = azurerm_resource_group.rg.location
-#   name                = "drogon-dojo-vn"
+#   name                = "dragon-dojo-vn"
 #   resource_group_name = azurerm_resource_group.rg.name
 #   address_space = ["10.0.0.0/16"]
 # }
 #
 # resource "azurerm_subnet" "subnet" {
 #   address_prefixes = ["10.0.2.0/24"]
-#   name                 = "dragon-doje-subnet"
+#   name                 = "dragon-dojo-subnet"
 #   resource_group_name  = azurerm_resource_group.rg.name
 #   virtual_network_name = azurerm_virtual_network.vn.name
 #
