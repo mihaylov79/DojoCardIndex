832a1a9c36abba830659f0d01d52d879fb8e6d90
mihaylov79 <misho.mihaylov@gmail.com>
2026-09-16 02:46:37 +0300

    updated mail-svc url

--- src/main/java/cardindex/dojocardindex/notification/client/NotificationClient.java
+++ src/main/java/cardindex/dojocardindex/notification/client/NotificationClient.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-//@FeignClient(name = "mail-svc", url = "https://mail-svc-app-container-app.yellowdesert-a725fdfe.switzerlandnorth.azurecontainerapps.io/api/v1/notifications")
-@FeignClient(name = "mail-svc", url = "http://localhost:8081/api/v1/notifications")
+@FeignClient(name = "mail-svc", url = "https://mail-svc-app-container-app.bluewave-fd99efbb.polandcentral.azurecontainerapps.io/api/v1/notifications")
+//@FeignClient(name = "mail-svc", url = "http://localhost:8081/api/v1/notifications")
 public interface NotificationClient {
 
     @PostMapping("/preferences")
